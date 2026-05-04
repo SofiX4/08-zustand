6a07fab65b7c25cf4e8844f84be40d3ac5774272
Siela <boyko.sofania@gmail.com>
2026-05-04 09:06:09 +0200

    metadata

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,4 +1,25 @@
+import { Metadata } from "next";
 import css from "./not-found.module.css";
+
+export const metadata: Metadata = {
+  title: "NoteHub - Page Not Found",
+  description: "The page you are looking for does not exist.",
+  openGraph: {
+    title: `NoteHub - Page Not Found`,
+    description: "The page you are looking for does not exist.",
+    url: `https://notehub.com`,
+    siteName: "NoteHub",
+    images: [
+      {
+        url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
+        width: 1200,
+        height: 630,
+        alt: "NoteHub - Page Not Found",
+      },
+    ],
+    type: "website",
+  },
+};
 
 export default function NotFound() {
   return (
